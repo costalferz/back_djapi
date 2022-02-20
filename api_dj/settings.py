1790765a88e3dfe4eda717f8ca1202d87ec86c4e
Costalferz <costalferzus@gmail.com>
2022-02-20 23:29:25 +0700

    edit setting

--- api_dj/settings.py
+++ api_dj/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR / 'api_dj' / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
