fde1e7eeb93709526ebc1711e844d857934b2422
Costalferz <costalferzus@gmail.com>
2022-02-20 23:41:45 +0700

    fix setting

--- api_dj/settings.py
+++ api_dj/settings.py
@@ -131,8 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = BASE_DIR / 'api_dj' / 'static'
+STATIC_ROOT = BASE_DIR / 'api_dj' / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'api_dj' / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
