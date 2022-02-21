007d84ad8177cc86e1cbe7f30e004e12c67cb84d
Costalferz <costalferzus@gmail.com>
2022-02-21 13:22:55 +0700

    fix setting

--- api_dj/settings.py
+++ api_dj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')# or 'django-insecure-&%i56f$_+1@yks$fx#rx4d#a-md247-bie!qa5$cb%_dv)97zj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
