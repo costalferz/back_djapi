135deb4a12132aa5e1c317462f14649f02dd974a
costalferz <costalferzus@gmail.com>
2022-07-21 23:21:39 +0700

    update api

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,9 @@
 # serializers
 from rest_framework import serializers
+
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
